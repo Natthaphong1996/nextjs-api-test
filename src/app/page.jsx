45fff0cafa942823d03fc6c4a2ae731bce52742a
Natthaphong1996 <nattaphong.meesawad@gmail.com>
2024-04-06 13:07:41 +0700

    Get data from poke api
    Get data from poke api and create page Pokemon is use data from api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./component/Header";
+import PokeData from "./component/PokeData";
 
 export default function Home() {
   return (
     <>
         <Header/>
+        <PokeData/>
     </>
   );
 }
